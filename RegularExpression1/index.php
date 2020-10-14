b25f9d2e658e851495f5c535945b4dae5bf79bb6
makanidja <rade.kaznovac@gmail.com>
2020-10-14 11:03:12 +0200

    Regular expression 1

---
+++ RegularExpression1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regular Expression 1</title>
+</head>
+<body>
+    
+    <?php 
+    
+    $string = "My name is Daniel, Danilel is my name!";
+
+    echo preg_match("/M/", $string);
+    
+
+    
+    ?>
+
+</body>
+</html>
